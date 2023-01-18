560175e7137800ee15804bc467d1b2634d3a5d92
Dan Manela <danielmanela@gmail.com>
2023-01-18 15:36:37 +0000

    Add multivariate gaussian copula to function set

--- frugalCopyla/frugalCopyla/copula_functions.py
+++ frugalCopyla/frugalCopyla/copula_functions.py
@@ -1,7 +1,22 @@
+import jax.scipy.stats as jax_stats
 import jax.numpy as jnp
 
 
-def bivariate_gaussian_copula_lpdf(u, v, rho):
+def reshape_matrix(n_vars: int, rhos: list[float]) -> jnp.array:
+    assert int((n_vars ** 2 - n_vars) / 2) == len(rhos)
+    position_idx = 0
+    for n in reversed(range(1, n_vars+1)):
+        rhos.insert(position_idx, 1)
+        position_idx += n
+    corr = jnp.zeros((n_vars, n_vars))
+    triu = jnp.triu_indices(n_vars)
+    tril = jnp.tril_indices(n_vars, -1)
+    corr = corr.at[triu].set(rhos)
+    corr = corr.at[tril].set(corr.T[tril])
+    return corr
+
+
+def bivariate_gaussian_copula_lpdf(u, v, rho) -> float:
     u_2 = jnp.square(u)
     v_2 = jnp.square(v)
     rho_2 = jnp.square(rho)
@@ -10,7 +25,16 @@ def bivariate_gaussian_copula_lpdf(u, v, rho):
             rho_2 * (u_2 + v_2) - 2 * rho * u * v
         ) / (2 * (1 - rho_2))
     )
-	
+
+
+def multivar_gaussian_copula_lpdf(vars: dict, rhos: dict) -> float:
+    rvs = jnp.array(list(vars.values()))
+    mean = jnp.zeros(len(rvs_list))
+    cov = reshape_matrix(len(rvs_list), list(rhos.values()))
+    return jax_stats.multivariate_normal.logpdf(rvs, mean, cov)	
+
+
 _COPULA_TAGS = {
-	'bivariate_gaussian_copula': bivariate_gaussian_copula_lpdf
+	'bivariate_gaussian_copula': bivariate_gaussian_copula_lpdf,
+    'multivariate_gaussian_copula': multivar_gaussian_copula_lpdf
 }
