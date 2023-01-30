2f6370d59f72515e76537f92b29ef88c6d66896d
Dan Manela <danielmanela@gmail.com>
2023-01-30 15:54:08 +0000

    Add tests for copula functions

--- frugalCopyla/frugalCopyla/copula_functions.py
+++ frugalCopyla/frugalCopyla/copula_functions.py
@@ -11,25 +11,53 @@ def __reshape_matrix(n_vars: int, rhos: list[float], is_cholesky=False) -> jnp.a
     corr = jnp.zeros((n_vars, n_vars))
     triu = jnp.triu_indices(n_vars)
     tril = jnp.tril_indices(n_vars, -1)
-    corr = corr.at[triu].set(rhos).T
     if is_cholesky:
+        corr = corr.at[triu].set(rhos).T
         for i in range(len(corr)):
             corr = corr.at[i, i].set(jnp.sqrt(2 - corr[i, ].dot(corr[i, ])))
     else:
-        corr = corr.at[tril].set(corr.T[tril])
+        corr = corr.at[triu].set(rhos).T.at[triu].set(rhos)
     return corr
 
 
 def multivar_gaussian_copula_lpdf(vars: dict, rhos: dict) -> float:
     rvs = jnp.array(list(vars.values()))
     mean = jnp.zeros(len(rvs))
-    cov = __reshape_matrix(len(rvs), list(rhos.values()), is_cholesky=True)
-    return jax_stats.multivariate_normal.logpdf(rvs, mean, cov)	
+    cov = __reshape_matrix(len(rvs), list(rhos.values()), is_cholesky=False)
+    llhood = jax_stats.multivariate_normal.logpdf(rvs, mean, cov)	
+    if not jnp.isnan(llhood).any():
+        return llhood
+    else:
+        return -jnp.inf
+
+
+#def bivariate_gaussian_copula_lpdf(u, v, rho) -> float:
+def bivariate_gaussian_copula_lpdf(vars: dict, rhos: dict) -> float:
+    u, v = vars.values()
+    rho = list(rhos.values())[0]
+    u_2 = jnp.square(u)
+    v_2 = jnp.square(v)
+    rho_2 = jnp.square(rho)
+    llhood = (
+        -0.5 * (1 - rho_2) - (
+            rho_2 * (u_2 + v_2) - 2 * rho * u * v
+        ) / (2 * (1 - rho_2))
+    )
+    if not jnp.isnan(llhood).any():
+        return llhood
+    else:
+        return -jnp.inf
+    
 
 
 def chol_multivar_gaussian_copula_lpdf(vars: dict, rhos: dict) -> float:
     rvs = jnp.array(list(vars.values()))
     mean = jnp.zeros(len(rvs))
-    chol_cov = __reshape_matrix(len(rvs), list(rhos.values()), is_cholesky=False)
+    chol_cov = __reshape_matrix(len(rvs), list(rhos.values()), is_cholesky=True)
     cov = jnp.matmul(chol_cov, chol_cov.T)
-    return jax_stats.multivariate_normal.logpdf(rvs, mean, cov)	
+    llhood = jax_stats.multivariate_normal.logpdf(rvs, mean, cov)	
+    if not jnp.isnan(llhood).any():
+        return llhood
+    else:
+        return -jnp.inf
+    
---
+++ frugalCopyla/tests/test_copula_functions.py
@@ -0,0 +1,41 @@
+import jax.numpy as jnp
+import pytest
+
+import frugalCopyla.copula_functions as cops
+
+
+def test_binary_non_chol_reshape_matrix():
+	n_vars = 2
+	rhos = [0.5]
+	outcome_corr = cops.__reshape_matrix(n_vars, rhos, is_cholesky=False)
+	target_outcome = jnp.array([[1,0.5], [0.5, 1]]) 
+	assert (outcome_corr == target_outcome).all()
+
+
+def test_three_class_non_chol_reshape_matrix():
+	n_vars = 3
+	rhos = [0.1, 0.2, 0.3]
+	outcome_corr = cops.__reshape_matrix(n_vars, rhos, is_cholesky=False)
+	target_outcome = jnp.array([[1,0.1, 0.2], [0.1, 1, 0.3], [0.2, 0.3, 1]]) 
+	assert (outcome_corr == target_outcome).all()
+
+
+def test_four_class_non_chol_reshape_matrix():
+	n_vars = 4
+	rhos = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+	outcome_corr = cops.__reshape_matrix(n_vars, rhos, is_cholesky=False)
+	target_outcome = jnp.array([
+		[1, 0.1, 0.2, 0.3],
+		[0.1, 1, 0.4, 0.5],
+		[0.2, 0.4, 1, 0.6],
+		[0.3, 0.5, 0.6, 1]
+	]) 
+	assert (outcome_corr == target_outcome).all()
+
+
+def test_binary_chol_reshape_matrix():
+	pass
+
+
+def test_three_class_chol_reshape_matrix():
+	pass
