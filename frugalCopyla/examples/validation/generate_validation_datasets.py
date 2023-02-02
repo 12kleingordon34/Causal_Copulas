cb46ee1a98ed5e13d4de14999a81e20bb95f5b3d
Dan Manela <danielmanela@gmail.com>
2023-02-02 16:42:09 +0000

    Add multicause confounders data generation

--- frugalCopyla/examples/validation/generate_validation_datasets.py
+++ frugalCopyla/examples/validation/generate_validation_datasets.py
@@ -32,6 +32,20 @@ def main():
 				'link': {'rho_ZY': None}
 			}
 		}
+		multiconfounder_gaussian_cop = {
+			'Z1': {'dist': dist.Normal, 'formula': {'loc': 'Z1 ~ 1', 'scale': 'Z1 ~ 1'}, 'coeffs': {'loc': [0.], 'scale': [1.]}, 'link': {}},
+			'Z2': {'dist': dist.Normal, 'formula': {'loc': 'Z2 ~ Z1', 'scale': 'Z1 ~ 1'}, 'coeffs': {'loc': [0., 0.5], 'scale': [1.]}, 'link': {}},
+			#'X': {'dist': dist.Normal, 'formula': {'loc': 'X ~ Z1 + Z2', 'scale': 'X ~ Z1'}, 'coeffs': {'loc': [0., rho, 0.5], 'scale': [1., -jnp.square(rho)]}, 'link': {'scale': jnp.sqrt}},
+			'X': {'dist': dist.Normal, 'formula': {'loc': 'X ~ Z1 + Z2', 'scale': 'X ~ 1'}, 'coeffs': {'loc': [0., rho, 0.5], 'scale': [1.]}, 'link': {'scale': jnp.sqrt}},
+			'Y': {'dist': dist.Normal, 'formula': {'loc': 'Y ~ X', 'scale': 'Y ~ 1'}, 'coeffs': {'loc': [-0.5, 1], 'scale': [1.]}, 'link': {}},
+			'copula': {
+				'class': copula_functions.multivar_gaussian_copula_lpdf,
+				'vars': ['Z1', 'Z2', 'Y'],
+				'formula': {'rho_Z1_Z2': 'cop ~ 1', 'rho_Z1_Y': 'cop ~ 1','rho_Z2_Y': 'cop ~ 1'},
+				'coeffs': {'rho_Z1_Z2': [0.0], 'rho_Z1_Y': [0.0],'rho_Z2_Y': [0.5]},
+				'link': {'rho_Z1_Z2': None, 'rho_Z1_Y': None,'rho_Z2_Y': None},
+			}
+		}
 
 		bernoulli_gaussian_cop = {
 			'Z': {'dist': dist.BernoulliProbs, 'formula': {'probs': 'Z ~ 1'}, 'coeffs': {'probs': [0.9]}, 'link': {}},
@@ -53,14 +67,14 @@ def main():
 			}
 		}
 
-		gaussian_data = generate_cop_data(multivar_gaussian_cop, 1_000_000, 'continuous', 0)
-		#bernoulli_data = generate_cop_data(bernoulli_gaussian_cop, 1_000_000, 'mixed', 0)
+		#gaussian_data = generate_cop_data(multivar_gaussian_cop, 1_000_000, 'continuous', 0)
 
-		gaussian_data.to_csv(f'./validation_datasets/multivar_gaussian/gaussian_cop_data_rho_{rho}.csv', index=False)
-		#bernoulli_data.to_csv(f'./validation_datasets/gaussian_bernoulli/bernoulli_cop_data_rho_{rho}.csv', index=False)
+		#gaussian_data.to_csv(f'./validation_datasets/multivar_gaussian/gaussian_cop_data_rho_{rho}.csv', index=False)
+		multiconfounder_data = generate_cop_data(multiconfounder_gaussian_cop, 1_00_000, 'continuous', 0)
+		multiconfounder_data.to_csv(f'./validation_datasets/multivar_gaussian/multiconfounder_gaussian_data_rho_{rho}.csv', index=False)
 	# Generate Didelez Data
-	didelez_data = generate_cop_data(didelez_cop, 1_000_000, 'mixed', 0)
-	didelez_data.to_csv(f'./validation_datasets/didelez_simulation.csv', index=False)
+	#didelez_data = generate_cop_data(didelez_cop, 1_000_000, 'mixed', 0)
+	#didelez_data.to_csv(f'./validation_datasets/didelez_simulation.csv', index=False)
 
 
 if __name__ == '__main__':
